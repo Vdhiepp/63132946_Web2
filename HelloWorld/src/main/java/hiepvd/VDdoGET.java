14f7d6fc92b98213ec3b9f26dbe93a0a71130eff
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-01-22 16:11:29 +0700

    ví dụ doGET

---
+++ HelloWorld/src/main/java/hiepvd/VDdoGET.java
@@ -0,0 +1,38 @@
+package hiepvd;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class VDdoGET
+ */
+public class VDdoGET extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public VDdoGET() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String value1 = request.getParameter("param1");
+		String value2 = request.getParameter("param2");
+		PrintWriter traVe = response.getWriter();
+		traVe.append("Gia tri tham so param1 =");
+		traVe.append(value1);
+		traVe.append("\n Gia tri tham so param2 =");
+		traVe.append(value2);
+	}
+
+}
