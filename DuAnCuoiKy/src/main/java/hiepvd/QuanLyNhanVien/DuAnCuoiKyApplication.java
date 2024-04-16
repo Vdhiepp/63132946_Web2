cfbc53aad4903f8b57fcecbe671b3759d2423c2f
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-04-16 15:53:45 +0700

    Dự án Quản lý nhân viên

---
+++ DuAnCuoiKy/src/main/java/hiepvd/QuanLyNhanVien/DuAnCuoiKyApplication.java
@@ -0,0 +1,13 @@
+package hiepvd.QuanLyNhanVien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnCuoiKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnCuoiKyApplication.class, args);
+	}
+
+}
