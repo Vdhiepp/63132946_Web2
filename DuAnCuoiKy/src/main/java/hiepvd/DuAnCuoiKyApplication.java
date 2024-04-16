3f6f070255abaf593402ccd533af22c028876414
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-04-16 15:06:59 +0700

    Tạo project dự án cuối kỳ

---
+++ DuAnCuoiKy/src/main/java/hiepvd/DuAnCuoiKyApplication.java
@@ -0,0 +1,13 @@
+package hiepvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnCuoiKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnCuoiKyApplication.class, args);
+	}
+
+}
