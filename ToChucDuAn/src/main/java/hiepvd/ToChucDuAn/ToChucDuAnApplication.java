5ce60d149fa314d7990ab563b9fe75df75cdabd3
Võ Đại Hiệp <vodaihiep209@gmail.com>
2024-04-23 14:44:12 +0700

    Tổ chức giao diện

---
+++ ToChucDuAn/src/main/java/hiepvd/ToChucDuAn/ToChucDuAnApplication.java
@@ -0,0 +1,13 @@
+package hiepvd.ToChucDuAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToChucDuAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToChucDuAnApplication.class, args);
+	}
+
+}
---
+++ ToChucDuAn/src/test/java/hiepvd/ToChucDuAn/ToChucDuAnApplicationTests.java
@@ -0,0 +1,13 @@
+package hiepvd.ToChucDuAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucDuAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
